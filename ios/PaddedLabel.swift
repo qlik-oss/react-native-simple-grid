3e9eb24b189a54ad3a28972350846e54b88a3dab
Vittorio Cellucci <vel@qlik.com>
2022-02-10 19:47:19 -0500

    fix(MD-2971): scale ios text

--- ios/PaddedLabel.swift
+++ ios/PaddedLabel.swift
@@ -17,7 +17,15 @@ class PaddedLabel: UILabel, SelectionsListener {
   let selectedBackgroundColor = ColorParser().fromCSS(cssString: "#009845")
   var selected = false
   var selectionsEngine: SelectionsEngine?
-
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame.integral)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override var intrinsicContentSize: CGSize {
     numberOfLines = 0       // don't forget!
     var s = super.intrinsicContentSize
