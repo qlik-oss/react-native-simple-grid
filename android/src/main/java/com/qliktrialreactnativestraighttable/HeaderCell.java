1dab2bf614988fe9af5545d6f791d336e1c6aeda
Vittorio Cellucci <vel@qlik.com>
2022-02-16 09:14:07 -0500

    fix(MD-2989): handle null sort direction

--- android/src/main/java/com/qliktrialreactnativestraighttable/HeaderCell.java
+++ android/src/main/java/com/qliktrialreactnativestraighttable/HeaderCell.java
@@ -1,10 +1,14 @@
 package com.qliktrialreactnativestraighttable;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.MotionEvent;
 
+import androidx.annotation.NonNull;
 
+
+@SuppressLint("ViewConstructor")
 public class HeaderCell extends androidx.appcompat.widget.AppCompatTextView {
   DataColumn column;
   CustomHorizontalScrollView scrollView;
@@ -24,7 +28,7 @@ public class HeaderCell extends androidx.appcompat.widget.AppCompatTextView {
 
   private void updateArrow() {
     if (column.active) {
-      if(column.sortDirection.compareToIgnoreCase("desc") == 0) {
+      if(column.sortDirection == null || column.sortDirection.compareToIgnoreCase("desc") == 0) {
         setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_arrow_drop_up_24, 0, 0, 0);
       } else {
         setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_arrow_drop_down_24, 0, 0, 0);
@@ -35,28 +39,39 @@ public class HeaderCell extends androidx.appcompat.widget.AppCompatTextView {
   }
 
   public void handleSingleTap() {
+    if(column.sortDirection == null && column.active) {
+      return;
+    }
     EventUtils.sendOnHeaderTapped(scrollView, column);
+    
   }
 
   public void handleDown() {
+    if(column.sortDirection == null && column.active) { 
+      return;
+    }
     this.setBackgroundColor(Color.LTGRAY);
     this.invalidate();
   }
 
+  @SuppressLint("ClickableViewAccessibility")
   @Override
-  public boolean onTouchEvent(MotionEvent e) {
+  public boolean onTouchEvent(@NonNull MotionEvent e) {
+    boolean result = false;
     switch (e.getAction()) {
       case MotionEvent.ACTION_DOWN:
         handleDown();
-        return true;
+        result = true;
+        break;
       case MotionEvent.ACTION_CANCEL:
       case MotionEvent.ACTION_UP:
         handleSingleTap();
-        return true;
+        result = true;
+        break;
       default:
         break;
     }
-    return false;
+    return result;
   }
 
 }
