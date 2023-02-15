71699cd092b44a0ed9c44cf7e04388243938f24c
Vittorio Cellucci <vittorio.cellucci@qlik.com>
2023-02-15 12:19:22 -0500

    fix: make first column background color opaque (#193)
    Co-authored-by: Vittorio Cellucci <vel@qlik.com>

--- ios/TableViewFactory.swift
+++ ios/TableViewFactory.swift
@@ -105,7 +105,7 @@ class TableViewFactory {
     firstColumnTableView.dataRange = 0..<1
     firstColumnTableView.isFirst = true
     firstColumnTableView.dynamicWidth = firstColumnTableView.widthAnchor.constraint(equalToConstant: width)
-    firstColumnTableView.backgroundColor = .white.withAlphaComponent(0.0)
+    firstColumnTableView.backgroundColor = .white
     let leadingAnchor = containerView.freezeFirstColumn ? horizontalScrollView.frameLayoutGuide.leadingAnchor : horizontalScrollView.leadingAnchor
 
     let constraints = [
