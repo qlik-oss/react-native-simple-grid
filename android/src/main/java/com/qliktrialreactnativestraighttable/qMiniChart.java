612d9377cfb104eac8495502c5794b861e6e603f
Vittorio Cellucci <vittorio.cellucci@qlik.com>
2023-11-15 14:33:27 -0500

    fix: android table crash when invalid json (#293)

--- android/src/main/java/com/qliktrialreactnativestraighttable/qMiniChart.java
+++ android/src/main/java/com/qliktrialreactnativestraighttable/qMiniChart.java
@@ -1,21 +1,40 @@
 package com.qliktrialreactnativestraighttable;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class qMiniChart {
-  public double qMin;
-  public double qMax;
-  public qMatrix matrix;
+  public double qMin = 0.0;
+  public double qMax = 0.0;
+  public qMatrix matrix = null;
+  
+  private double getValue(String key, ReadableMap data) {
+    if(data != null) {
+      if(data.hasKey(key)){
+        if(data.getType(key) == ReadableType.Number) {
+          return data.getDouble(key); 
+        }
+      }
+    }
+    return 0.0;
+  }
+  
   public qMiniChart(ReadableMap data) {
-    qMin = data.hasKey("qMin") ? data.getDouble("qMin") : 0.0;
-    qMax = data.hasKey("qMax") ? data.getDouble("qMax") : 0.0;
-    ReadableArray dataArray = data.hasKey("qMatrix") ? data.getArray("qMatrix") : null;
-    if(dataArray != null) {
-      matrix = new qMatrix(dataArray);
+    try {
+      qMin = getValue("qMin", data);
+      qMax = getValue("qMax", data);
+      ReadableArray dataArray = data.hasKey("qMatrix") ? data.getArray("qMatrix") : null;
+      if (dataArray != null) {
+        matrix = new qMatrix(dataArray);
+      }
+    } catch(Exception e) {
+      Log.d("qMiniChart", "Invalid");
     }
   }
 
